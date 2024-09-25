84a335cbbada1aede62b8b0e5228d9fb3813973c
nowadraco <Nowadraco@gmail.com>
2024-09-25 19:54:43 +0000

    ola mundo

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class Title extends React.Component {
   render() {
-    return (<h1 className="">{this.props.label}</h1>)
+    return (<h1>{this.props.label}</h1>);
   }
 }
 
 class Paragraph extends React.Component {
   render() {
     return (
-      <p className="">
-      {this.props.text}
+      <p>
+        {this.props.text}
       </p>
-    )
+    );
   }
 }
 
 class Main extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return  (
-      <div className="">
+    return (
+      <div>
         <Title label={this.props.title} />
-          <Paragraph text={this.props.text} />
+        <Paragraph text={this.props.text} />
       </div>
-    )
+    );
   }
 }
 
-
 ReactDOM.render(
-  <Main title="React" text="Caution: do not look into laser with remaining eye."></Main>,
+  <Main title="Olá Mundo" text="Bem-vindo ao React!" />,
   document.getElementById('react-app')
 );
